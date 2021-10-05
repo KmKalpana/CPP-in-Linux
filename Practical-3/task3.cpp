aea11b790709dc447f2bb187a88f555be52e5441
Km Kalpana <85006350+Kalpana-Kathait11@users.noreply.github.com>
2021-10-05 11:21:50 +0530

    Update task3.cpp

--- Practical-3/task3.cpp
+++ Practical-3/task3.cpp
@@ -1,3 +1,4 @@
+//Task-3: Take a char variable and use cin to take its value from the user, cout it and it will only return one character and loss rest of the data you have entered!
 #include <iostream>
 using namespace std;
 int main() {
